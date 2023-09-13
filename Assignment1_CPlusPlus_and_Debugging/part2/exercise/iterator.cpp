0c597c4149c6763998488121098e01ee829b0d5a
daniellee1011 <kauvoi1011@gmail.com>
2023-09-13 00:03:18 -0400

    [asn.] Implement assignment 1, part 2 - Task 9
    - Iterators

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/iterator.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/iterator.cpp
@@ -1,14 +1,28 @@
 // Compile with:
 // g++ -std=c++17 iterator.cpp -o prog
 #include <iostream>
+#include <vector>
 
-int main(){
+int main() {
 
-  	// Wow you are learning so much already by typing out these examples.
-	//
-	// Folks in the C++ world 'swear' by iterators.
-	// (And I sort of agree). But in this course, it's enough to just know they exist
-	// and you can start building a habit out of using them now.
+  // Wow you are learning so much already by typing out these examples.
+  //
+  // Folks in the C++ world 'swear' by iterators.
+  // (And I sort of agree). But in this course, it's enough to just know they
+  // exist and you can start building a habit out of using them now.
 
-	return 0;
+  std::vector<unsigned char> myData;
+
+  for (unsigned int i = 65; i < 91; ++i) {
+    myData.push_back(i);
+  }
+
+  std::vector<unsigned char>::iterator it = myData.begin();
+  std::vector<unsigned char>::iterator end = myData.end();
+
+  for (; it != end; it++) {
+    std::cout << *it;
+  }
+
+  return 0;
 }
