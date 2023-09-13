1f8fc0b290186187a5488be4e393c3790f15b49d
daniellee1011 <kauvoi1011@gmail.com>
2023-09-12 23:37:04 -0400

    [asn.] Implement assignment 1 , part 2 - Task 3
    : Writing and Reading data from Files

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/input.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/input.cpp
@@ -1,10 +1,40 @@
 // Compile with:
 // g++ -std=c++17 input.cpp -o prog
+#include <fstream>
 #include <iostream>
+#include <string>
 
-int main(){
+int main(int argc, char **argv) {
 
   // Learning how to read in values will be useful for the course.
 
-	return 0;
+  std::ifstream inFile;
+
+  // 1st argument: .\prog.exe
+  // 2nd argument: ..\..\..\common\objects\bunny_centered.obj
+  // Executable file is also counted.
+  if (argc != 2) {
+    std::cerr << "Oops, exactly two arguments needed.\n";
+    std::cerr << "ProgramName file_path.\n";
+    return 0;
+  }
+
+  inFile.open(argv[1]);
+
+  if (inFile.is_open()) {
+    std::string line;
+    while (getline(inFile, line)) {
+      std::cout << line << std::endl;
+    }
+  } else {
+    std::cout << "Filepath does not exist" << argv[1] << std::endl;
+    std::cout << "Make sure the path is relative to where you are executing "
+                 "your program"
+              << std::endl;
+  }
+
+  // Not closing can cause resource limits, data integrity, and locking issues.
+  inFile.close();
+
+  return 0;
 }
