33e06f870e94338ee308ced5b543623a4ac16da4
daniellee1011 <kauvoi1011@gmail.com>
2023-09-13 00:12:55 -0400

    [asn.] Implement assignment 1, part 2 - Task 11
    - Object-Oriented Programming and Classes

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/class.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/class.cpp
@@ -1,20 +1,42 @@
 // g++ -std=c++17 class.cpp -o prog
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
+class PPM {
+public:
+  PPM(unsigned int width, unsigned int height) {
+    pixelData = new unsigned char[width * height * 3];
+    dataSize = width * height * 3;
+  }
 
-int main(int argc, char** argv){
+  ~PPM() { delete[] pixelData; }
 
-    // You know what you need to do... :) Type it out.
-    //
-    //
-    // Here's something I found funny as a reward:
-    // https://stackoverflow.blog/2021/04/19/how-often-do-people-actually-copy-and-paste-from-stack-overflow-now-we-know/
-    //
-    // (Stack overflow is good too--but copying and pasting doesn't help us learn)
-    //
-    //
-    // Anyway, at this point you know most of the C++ you'll need to get you through the course.
+  void printPixelDataSize() {
+    std::cout << "Size of pixelData: " << dataSize << " bytes" << std::endl;
+  }
 
-    return 0;
+private:
+  uint8_t *pixelData;
+  unsigned int dataSize;
+};
+
+int main(int argc, char **argv) {
+
+  // You know what you need to do... :) Type it out.
+  //
+  //
+  // Here's something I found funny as a reward:
+  // https://stackoverflow.blog/2021/04/19/how-often-do-people-actually-copy-and-paste-from-stack-overflow-now-we-know/
+  //
+  // (Stack overflow is good too--but copying and pasting doesn't help us learn)
+  //
+  //
+  // Anyway, at this point you know most of the C++ you'll need to get you
+  // through the course.
+
+  PPM myPPM(64, 128);
+  myPPM.printPixelDataSize();
+
+  return 0;
 }
