8c48c1ea11d32f88b4e27f010fc2a66d6485b534
Daniel <40425358+daniellee1011@users.noreply.github.com>
2023-09-12 23:11:03 -0400

    [asn.] Implement assignment 1 , part 2 - datasize.cpp

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/datasize.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/datasize.cpp
@@ -1,8 +1,9 @@
 // Compile with:
 // g++ -std=c++17 datasize.cpp -o prog
+#include <cstdint>
 #include <iostream>
 
-int main(){
+int main() {
 
   // Type out the example.
   // Actually type it out--that is how you will learn and
@@ -10,5 +11,16 @@ int main(){
   // Do not copy & paste...actually type it out.
   // Change some values, play around, experiment.
 
-	return 0;
+  std::cout << "Data size of int in bytes = " << sizeof(int) << std::endl;
+
+  std::cout << "Data size of char in bytes = " << sizeof(char) << std::endl;
+
+  std::cout << "Data size of uint8_t in bytes = " << sizeof(uint8_t)
+            << std::endl;
+
+  std::cout << "Data size of float in bytes = " << sizeof(float) << std::endl;
+
+  std::cout << "Data size of double in bytes = " << sizeof(double) << std::endl;
+
+  return 0;
 }
