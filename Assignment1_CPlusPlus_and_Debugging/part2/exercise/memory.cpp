166026dc5a40cc64cbca66cc76a6518c7ef24831
daniellee1011 <kauvoi1011@gmail.com>
2023-09-13 00:00:41 -0400

    [asn.] Implement assignment 1, part 2 - Task 8
    - Memory (new and delete)

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/memory.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/memory.cpp
@@ -1,17 +1,27 @@
 // g++ -std=c++17 memory.cpp -o prog
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
-
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 
   // We'll have to get comfortable with working with memory.
   //
   // Eventually we'll be passing around memory from the CPU to GPU and back.
   //
-  // That's why I am so adamant you type out the examples now and get practice in early.
+  // That's why I am so adamant you type out the examples now and get practice
+  // in early.
   //
   // You can add more to them too--that's the best way to learn!
+  uint8_t *contiguous_chunk_of_memory = new uint8_t[80];
 
-    return 0;
+  uint8_t *one_item = new uint8_t;
+
+  std::cout << "sizeof(contiguous...) = " << sizeof(contiguous_chunk_of_memory)
+            << std::endl;
+
+  delete one_item;
+  delete[] contiguous_chunk_of_memory;
+
+  return 0;
 }
