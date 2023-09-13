2607833bed6c0030c27466a5fe6a58af7560a049
daniellee1011 <kauvoi1011@gmail.com>
2023-09-13 00:05:05 -0400

    [asn.] Implement assignment 1, part 2 - Task 10
    - One more look at vectors

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/vector_data.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/vector_data.cpp
@@ -2,14 +2,26 @@
 #include <iostream>
 #include <vector>
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 
   // Making you type out this example because
   // it will come in handy...
   //
-  // In fact, for all these examples I picked out a 
+  // In fact, for all these examples I picked out a
   // minimum subset of C++ that'll get you through
   // the course.
 
-    return 0;
+  std::vector<int> myData;
+
+  for (int i = 0; i < 43; i++) {
+    myData.push_back(i);
+  }
+
+  int *internalArray = myData.data();
+
+  for (int i = 0; i < myData.size(); i++) {
+    std::cout << internalArray[i] << "\n";
+  }
+
+  return 0;
 }
