976b56831cb5376b29aaecf01bddd0ca1fcb84ec
daniellee1011 <kauvoi1011@gmail.com>
2023-09-12 23:40:50 -0400

    [asn.] Implement assignment 1, part 2 - Task 4
    - Working with Strings

--- Assignment1_CPlusPlus_and_Debugging/part2/exercise/string.cpp
+++ Assignment1_CPlusPlus_and_Debugging/part2/exercise/string.cpp
@@ -1,11 +1,29 @@
 // Compile with:
 // g++ -std=c++17 string.cpp -o prog
 #include <iostream>
+#include <string>
 
-int main(){
+int main() {
 
   // Type out the example.
   // We'll work with std::strings a lot to parse and manipulate data.
 
-	return 0;
+  std::string fullName = "Dongwook Lee";
+
+  std::string firstName = fullName.substr(0, 8);
+  std::string lastName = fullName.substr(9, 13);
+
+  std::cout << "First: " << firstName << std::endl;
+  std::cout << "Last: " << lastName << std::endl;
+
+  unsigned int i = 0;
+  while (i < fullName.length()) {
+    if (fullName.at(i) == ' ') {
+      break;
+    }
+    std::cout << fullName[i];
+    ++i;
+  }
+
+  return 0;
 }
