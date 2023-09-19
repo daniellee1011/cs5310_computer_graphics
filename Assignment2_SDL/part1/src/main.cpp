dcdc16a642fe8e1d29fb04e17eb0580246c4bc10
daniellee1011 <kauvoi1011@gmail.com>
2023-09-19 15:51:53 -0400

    [asn. 2] part 1 - Task 5
    - Pressing the key one time will change the color

--- Assignment2_SDL/part1/src/main.cpp
+++ Assignment2_SDL/part1/src/main.cpp
@@ -16,80 +16,86 @@
 //       global variable.
 // Note: 'globals' generally are bad as they can clutter
 //       the program. Havin '1' is sometimes okay.
-SDLGraphicsProgram gSDLGraphicsProgram(1280,720,1);
+SDLGraphicsProgram gSDLGraphicsProgram(1280, 720, 1);
 
 // Called once before the main loop
 // Useful for any 'setup' that you need to do.
-void PreLoop(void){
-    std::cout << "Called before the loop one time!\n";
-}
+void PreLoop(void) { std::cout << "Called before the loop one time!\n"; }
 
 // Handle any 'user input' here.
-void Input(void){
-	// Event handler that handles various events in SDL
-    // that are related to input and output
-    SDL_Event e;
-    // Enable text input
-    // SDL_StartTextInput();
-	//Handle events on queue
-	while(SDL_PollEvent( &e ) != 0){
-		// User posts an event to quit
-		// An example is hitting the "x" in the corner of the window.
-		// Set our 'flag' to true so that we quit executing the Loop() function..
-		if(e.type == SDL_QUIT){
-            gSDLGraphicsProgram.TerminateLoop();
-		}
-        // TODO Refactor the code such that you can change the background
-        // TODO color based on the keypress 1, 2, or 3 to red, green, and blue.
-        // TODO You can refactor however you like. A sample of finding a keydown
-        // TODO event is otherwise shown.
-        // TODO For help: 
-        // TODO https://lazyfoo.net/tutorials/SDL/04_key_presses/index.php
-        // Or a video tutorial on keyboard input: https://www.youtube.com/watch?v=piEekEUqvKY
-        if(e.type == SDL_KEYDOWN){
-            gSDLGraphicsProgram.SetClearColor(0.5,0.0,0.0,1.0);
-        }else{
-            gSDLGraphicsProgram.SetClearColor(0.0,0.5,0.0,1.0);
-        }
+void Input(void) {
+  // Event handler that handles various events in SDL
+  // that are related to input and output
+  SDL_Event e;
+  // Enable text input
+  // SDL_StartTextInput();
+  // Handle events on queue
+  while (SDL_PollEvent(&e) != 0) {
+    // User posts an event to quit
+    // An example is hitting the "x" in the corner of the window.
+    // Set our 'flag' to true so that we quit executing the Loop() function..
+    if (e.type == SDL_QUIT) {
+      gSDLGraphicsProgram.TerminateLoop();
+    }
+    // TODO Refactor the code such that you can change the background
+    // TODO color based on the keypress 1, 2, or 3 to red, green, and blue.
+    // TODO You can refactor however you like. A sample of finding a keydown
+    // TODO event is otherwise shown.
+    // TODO For help:
+    // TODO https://lazyfoo.net/tutorials/SDL/04_key_presses/index.php
+    // Or a video tutorial on keyboard input:
+    // https://www.youtube.com/watch?v=piEekEUqvKY
+    if (e.type == SDL_KEYDOWN) {
+      std::cout << "A key has been pressed" << std::endl;
+      switch (e.key.keysym.sym) {
+      case SDLK_1:
+        std::cout << "1 key has been pressed" << std::endl;
+        gSDLGraphicsProgram.SetClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        break;
+      case SDLK_2:
+        std::cout << "2 key has been pressed" << std::endl;
+        gSDLGraphicsProgram.SetClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        break;
+      case SDLK_3:
+        std::cout << "3 key has been pressed" << std::endl;
+        gSDLGraphicsProgram.SetClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        break;
+      }
+    }
+  } // End SDL_PollEvent loop.
 
-	} // End SDL_PollEvent loop.
-
-    //Disable text input
-    //SDL_StopTextInput();
+  // Disable text input
+  // SDL_StopTextInput();
 }
 
 // Per frame update called in the main loop.
-void Update(void){
-
-}
-
+void Update(void) {}
 // Per frame render called in the main loop
 // Used for rendering geometry/graphics to the screen.
-void Render(void){
-    // Sets up the default rendering state in
-    // OpenGL
-    gSDLGraphicsProgram.SetDefaultOpenGLState();
+void Render(void) {
+  // Sets up the default rendering state in
+  // OpenGL
+  gSDLGraphicsProgram.SetDefaultOpenGLState();
 }
 
-
 // Default entry point into any C++ program
-int main(int argc, char* argv[]){
-	// Starting program
-	std::cout << "Entry Point to Program\n";
-	// Confirm our OpenGL Version Number
-	gSDLGraphicsProgram.GetOpenGLVersionInfo();
-    // Setup each of your function pointers before
-    // the main loop.
-    gSDLGraphicsProgram.SetPreLoopCallback(PreLoop);
-    gSDLGraphicsProgram.SetInputCallback(Input);
-    gSDLGraphicsProgram.SetUpdateCallback(Update);
-    gSDLGraphicsProgram.SetRenderCallback(Render);
-	// Run our program forever in an infinite loop
-    // Your 'infinite loop' will calle each of the
-    // callsbacks in the order of Input->Update->Render
-    // (PreLoopCallback is called once before the loop)
-	gSDLGraphicsProgram.Loop();
-	// When our program ends, it will exit scope, the
-	// destructor will then be called and clean up the program.
-	return 0;
+int main(int argc, char *argv[]) {
+  // Starting program
+  std::cout << "Entry Point to Program\n";
+  // Confirm our OpenGL Version Number
+  gSDLGraphicsProgram.GetOpenGLVersionInfo();
+  // Setup each of your function pointers before
+  // the main loop.
+  gSDLGraphicsProgram.SetPreLoopCallback(PreLoop);
+  gSDLGraphicsProgram.SetInputCallback(Input);
+  gSDLGraphicsProgram.SetUpdateCallback(Update);
+  gSDLGraphicsProgram.SetRenderCallback(Render);
+  // Run our program forever in an infinite loop
+  // Your 'infinite loop' will calle each of the
+  // callsbacks in the order of Input->Update->Render
+  // (PreLoopCallback is called once before the loop)
+  gSDLGraphicsProgram.Loop();
+  // When our program ends, it will exit scope, the
+  // destructor will then be called and clean up the program.
+  return 0;
 }
