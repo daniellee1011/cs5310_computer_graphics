243dfed76990396dc1549b9c57dec12569ef8791
MikeShah <mshah.475@gmail.com>
2023-10-06 10:47:40 -0400

    adding: New Directory

---
+++ Assignment5_RotationAndCamera/part1/shaders/vert.glsl
@@ -0,0 +1,27 @@
+#version 410 core
+// From Vertex Buffer Object (VBO)
+// The only thing that can come 'in', that is
+// what our shader reads, the first part of the
+// graphics pipeline.
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 vertexColors;
+
+// Uniform variables
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_ViewMatrix;
+uniform mat4 u_Projection; // We'll use a perspective projection
+
+// Pass vertex colors into the fragment shader
+out vec3 v_vertexColors;
+
+void main()
+{
+  v_vertexColors = vertexColors;
+
+  // TODO: Utilize your MVP matrix
+  vec4 newPosition =  vec4(position,1.0f);
+                                    // Don't forget 'w=1.0f'
+	gl_Position = vec4(newPosition.x, newPosition.y, newPosition.z, newPosition.w);
+}
+
+
