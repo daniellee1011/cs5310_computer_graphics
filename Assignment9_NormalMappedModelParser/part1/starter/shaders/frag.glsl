70d885f9da4684951b06d1df79fdaf799e19b3ca
daniellee1011 <kauvoi1011@gmail.com>
2023-11-10 15:01:28 -0500

    [asn. 9] Update shaders in starter dir to render wall with normal map

--- Assignment9_NormalMappedModelParser/part1/starter/shaders/frag.glsl
+++ Assignment9_NormalMappedModelParser/part1/starter/shaders/frag.glsl
@@ -11,21 +11,36 @@ in vec3 TangentLightPos;
 in vec3 TangentViewPos;
 in vec3 TangentFragPos;
 
-
 // If we have texture coordinates, they are stored in this sampler.
-uniform sampler2D u_DiffuseMap; 
-uniform sampler2D u_NormalMap; 
+uniform sampler2D u_DiffuseMap;
+uniform sampler2D u_NormalMap;
 
 void main()
 {
-	// Store the texture coordinates
-	vec3 normal = texture(u_NormalMap, v_texCoord).rgb;
-	vec3 color =  texture(u_DiffuseMap, v_texCoord).rgb;
+    vec3 normal = texture(u_NormalMap, v_texCoord).rgb;
+    normal = normalize(normal * 2.0 - 1.0); // Convert from [0,1] to [-1,1]
 
-	if(v_texCoord.y > 0.5){
-	    FragColor = vec4(normal,1.0);
-	}else{
-	    FragColor = vec4(color,1.0);
-	}
+    // Apply TBN matrix to normals
+    vec3 fragNormal = normal;
+
+    // Get direction to light source
+    vec3 lightDir = normalize(TangentLightPos - TangentFragPos);
+
+    // Calculate the diffuse impact by dot product of fragment normal and light direction
+    float diff = max(dot(fragNormal, lightDir), 0.0);
+
+    // Calculate the specular impact
+    vec3 viewDir = normalize(-TangentFragPos); // View direction in tangent space
+    vec3 reflectDir = reflect(-lightDir, fragNormal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32); // Hard-coded shininess
+
+    vec3 textureColor = texture(u_DiffuseMap, v_texCoord).rgb;
+    vec3 ambient = 0.1 * textureColor; // Ambient color (hard-coded)
+    vec3 diffuse = diff * textureColor; // Diffuse color
+    vec3 specular = vec3(0.5) * spec; // Specular color (hard-coded)
+
+    // Combine diffuse and specular components
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
 // ==================================================================
--- Assignment9_NormalMappedModelParser/part1/starter/shaders/vert.glsl
+++ Assignment9_NormalMappedModelParser/part1/starter/shaders/vert.glsl
@@ -3,11 +3,11 @@
 // Read in our attributes stored from our vertex buffer object
 // We explicitly state which is the vertex information
 // (The first 3 floats are positional data, we are putting in our vector)
-layout(location=0)in vec3 position; 
-layout(location=1)in vec3 normals; // Our second attribute - normals.
-layout(location=2)in vec2 texCoord; // Our third attribute - texture coordinates.
-layout(location=3)in vec3 tangents; // Our third attribute - texture coordinates.
-layout(location=4)in vec3 bitangents; // Our third attribute - texture coordinates.
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 normals;
+layout(location=2) in vec2 texCoord;
+layout(location=3) in vec3 tangents;
+layout(location=4) in vec3 bitangents;
 
 // If we have texture coordinates we can now use this as well
 out vec3 FragPos;
@@ -19,18 +19,31 @@ out vec3 TangentFragPos;
 // If we are applying our camera, then we need to add some uniforms.
 // Note that the syntax nicely matches glm's mat4!
 
-uniform mat4 modelTransformMatrix; // Object space
+uniform mat4 modelTransformMatrix;
+uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-uniform vec3 lightPos; // Our light source position from where light is hitting this object
-uniform vec3 viewPos;  // Where our camera is
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
+	gl_Position = projectionMatrix * modelTransformMatrix * vec4(position, 1.0f);
 
-	gl_Position = projectionMatrix * modelTransformMatrix * vec4(position, 1.0f);;
+    vec4 FragPos4 = modelTransformMatrix * vec4(position, 1.0);
+    FragPos = FragPos4.xyz;
 
-  	// Store the texture coordinaets which we will output to
-  	// the next stage in the graphics pipeline.
-  	v_texCoord = texCoord;
+    v_texCoord = texCoord;
+
+    // Compute the TBN matrix
+    mat3 normalMatrix = transpose(inverse(mat3(modelTransformMatrix)));
+    vec3 T = normalize(normalMatrix * tangents);
+    vec3 B = normalize(normalMatrix * bitangents);
+    vec3 N = normalize(normalMatrix * normals);
+    mat3 TBN = mat3(T, B, N);
+
+    // Transform the light and view position to tangent space
+    TangentLightPos = TBN * (lightPos - FragPos);
+    TangentViewPos = TBN * (viewPos - FragPos);
+    TangentFragPos = TBN * FragPos;
 }
 // ==================================================================
