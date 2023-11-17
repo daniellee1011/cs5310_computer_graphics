1e63335d25df59106c6137fde786dc8df36081c4
daniellee1011 <kauvoi1011@gmail.com>
2023-11-17 14:00:30 -0500

    [asn. 10] Post-processing: Kernel effects

--- Assignment10_fbo/part1/shaders/fboFrag.glsl
+++ Assignment10_fbo/part1/shaders/fboFrag.glsl
@@ -12,10 +12,37 @@ in vec2 v_texCoord; // Import our texture coordinates from vertex shader
 // The final output color of each 'fragment' from our fragment shader.
 out vec4 FragColor;
 
+const float offset = 1.0 / 300.0;  
+
 void main()
 {
-    FragColor = texture(u_DiffuseMap, v_texCoord);
-    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-    FragColor = vec4(average, average, average, 1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
+
+        float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(u_DiffuseMap, v_texCoord.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    FragColor = vec4(col, 1.0);
 }
 // ==================================================================
