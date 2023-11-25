91fb04eed4727679eb3836e3ce2a27ea82becb07
daniellee1011 <kauvoi1011@gmail.com>
2023-11-24 22:13:37 -0500

    [asn. 11] Set up for compile success

--- Assignment11_SceneGraph/part1/include/SDLGraphicsProgram.hpp
+++ Assignment11_SceneGraph/part1/include/SDLGraphicsProgram.hpp
@@ -1,6 +1,6 @@
 /** @file SDLGraphicsProgram.hpp
  *  @brief SDL Class used to setup input and setup of OpenGL.
- *  
+ *
  *  This class is used for the initialization of SDL.
  *
  *  @author Mike
@@ -17,46 +17,40 @@
 // This gives an example of how a programmer
 // may support multiple platforms with different
 // dependencies.
-#if defined(LINUX) || defined(MINGW)
-    #include <SDL2/SDL.h>
-#else // This works for Mac
-    #include <SDL.h>
-#endif
+#include <SDL2/SDL.h>
 
 // Include the 'Renderer.hpp' which deteremines what
 // the graphics API is going to be for OpenGL
 #include "Renderer.hpp"
-
 
 // Purpose:
 // This class sets up a full graphics program using SDL
 //
 //
 //
-class SDLGraphicsProgram{
+class SDLGraphicsProgram {
 public:
-
-    // Constructor
-    SDLGraphicsProgram(int w, int h);
-    // Destructor
-    ~SDLGraphicsProgram();
-    // Setup OpenGL
-    bool InitGL();
-    // Loop that runs forever
-    void Loop();
-    // Get Pointer to Window
-    SDL_Window* GetSDLWindow();
-    // Helper Function to Query OpenGL information.
-    void GetOpenGLVersionInfo();
+  // Constructor
+  SDLGraphicsProgram(int w, int h);
+  // Destructor
+  ~SDLGraphicsProgram();
+  // Setup OpenGL
+  bool InitGL();
+  // Loop that runs forever
+  void Loop();
+  // Get Pointer to Window
+  SDL_Window *GetSDLWindow();
+  // Helper Function to Query OpenGL information.
+  void GetOpenGLVersionInfo();
 
 private:
-	// The Renderer responsible for drawing objects
-	// in OpenGL (Or whatever Renderer you choose!)
-	Renderer* m_renderer;
-    // The window we'll be rendering to
-    SDL_Window* m_window ;
-    // OpenGL context
-    SDL_GLContext m_openGLContext;
+  // The Renderer responsible for drawing objects
+  // in OpenGL (Or whatever Renderer you choose!)
+  Renderer *m_renderer;
+  // The window we'll be rendering to
+  SDL_Window *m_window;
+  // OpenGL context
+  SDL_GLContext m_openGLContext;
 };
 
 #endif
